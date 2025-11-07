cb10a1b3f95f1e74edb8446d8c84461cb9d061cd
azmibasri <azmibasri391@gmail.com>
2025-11-07 10:50:23 +0700

    ganti db

--- db_connect.php
+++ db_connect.php
@@ -3,9 +3,9 @@ $servername = getenv('DB_HOST') ?: 'localhost';
 $username = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASS') ?: '';
 $database = getenv('DB_NAME') ?: 'db_auth';
+$port = getenv('DB_PORT') ?: 3306;
 
-$conn = new mysqli($servername, $username, $password, $database);
-
+$conn = new mysqli($servername, $username, $password, $database, $port);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
